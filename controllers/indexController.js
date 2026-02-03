b73800a0f3e09dbe2a062d46b75674046468dbc5
Jacob Densford <hello@jacobdensford.com>
2026-02-03 10:52:53 -0500

    Add folder to upload

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,10 +1,23 @@
-function getIndex(req, res) {
+const { prisma } = require('../lib/prisma');
+
+async function getIndex(req, res) {
     if (!req.user) {
         res.redirect('log-in');
     } else {
+        const user = await prisma.user.findUnique({
+            where: {
+                username: req.user.username,
+            },
+        });
+        const folders = await prisma.folder.findMany({
+            where: {
+                userId: user.id,
+            }
+        });
         res.render('index', {
             title: 'File Uploader',
             username: req.user.username,
+            folders: folders,
         });
     }
 }
